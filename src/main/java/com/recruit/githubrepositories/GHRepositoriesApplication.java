a0334b2de0ca2c9603b9077cb6af21def05f1b5b
Rafał Kotusiewicz <rk@buzzlers.com>
2020-01-21 15:48:30 +0100

    Fix code formatting

--- src/main/java/com/recruit/githubrepositories/GHRepositoriesApplication.java
+++ src/main/java/com/recruit/githubrepositories/GHRepositoriesApplication.java
@@ -18,38 +18,38 @@ import java.util.Collections;
 @SpringBootApplication
 public class GHRepositoriesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GHRepositoriesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GHRepositoriesApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfiguration {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.recruit.githubrepositories"))
-				.paths(PathSelectors.ant("/repositories/**"))
-				.build()
-				.apiInfo(apiInfo());
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.recruit.githubrepositories"))
+                .paths(PathSelectors.ant("/repositories/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfo(
-				"RecruitApp REST API",
-				"More descriptive description...",
-				"API test",
-				"Terms of service",
-				new Contact("Rafał Kotusiewicz", "https://www.tt.com.pl", "Rafal.Kotusiewicz@tt.com.pl"),
-				"License of API", "API license URL", Collections.emptyList());
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "RecruitApp REST API",
+                "More descriptive description...",
+                "API test",
+                "Terms of service",
+                new Contact("Rafał Kotusiewicz", "https://www.tt.com.pl", "Rafal.Kotusiewicz@tt.com.pl"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
 }
 
